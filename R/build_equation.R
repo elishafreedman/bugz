c51c4c934b25e6b933e2118dba5f44ce17dbfd2f
Elisha Freedman <freedmanelisha@gmail.com>
2021-07-28 20:12:04 +1000

    first iteration

---
+++ R/build_equation.R
@@ -0,0 +1,171 @@
+#N = the total number of host individuals
+
+#K = carrying capacity
+
+#k = the number of endosymbionts
+
+#beta = the baseline transmission rate
+
+#sigma = how strongly transmission rates decline based on which type of
+#endosymbiont there is
+
+
+# Function creating the ODE equations
+
+#single transmission rate model added here represented by the argument "endosymbiont type"
+
+build_equations <- function(endo_s = endo_species, endo_no = endo_number){
+  k <- endo_no+1 #number of endosymbionts (including the uninfected state)
+
+
+  #max types = 2 at the moment
+  if(endo_s == 1){
+    Ns <- rep("", k)
+    for(i in 0:(k-1)){
+      Ns[i + 1] <- paste0("N", i)
+    }
+    NN <- paste(Ns, collapse = "+")
+
+    # left sides of equations:
+    leftSides <- paste0("d", Ns)
+
+    # matrix of terms for demography:
+    matD <- matrix(rep(NA, k), nrow = 1)
+    for(i in 0:(k-1)) {
+      matD[i+1] <- paste0("lambda*N", i, "*(1 - (", NN, ")/K) - mu*N",i)
+    }
+    dim(matD) <- NULL
+
+    # matrix of terms for the  influx force of infection
+
+    # matrix for transmission events:
+
+    matT <- matrix(rep(NA, k), nrow = 1)
+
+    # total number of species with symbiont A:
+
+    matT <- matrix(rep("", k), nrow = 1)
+    for(i in 0:(k-1)){
+      if(i>0){ # influx of A symbionts
+        matT[i+1] <- paste0("+betaA*sigmaA*N", i-1)
+      }
+      if(i<(k-1)) {#outflux of A endosymbionts
+        matT[i+1] <- paste0("-betaA*sigmaA*N", i)
+      }
+    }
+    matL <- matrix(rep(NA, k), nrow = 1)
+    for(i in 0:(k-1)){
+      matL[i+1] <- paste0("(-nuA)*N", i)
+    }
+
+    # matrix of terms for influx due to loss of symbionts:
+    matG <- matrix(rep("", k), nrow = 1)
+    for(i in 0:(k-1)) {
+      if (i<(k-1))
+        matG[i+1] <- paste0("nuA*N", i+1)
+      else
+        matG[i+1] <- "0"
+    }
+  }
+
+
+
+
+
+  else ###### two  types ########
+  if(endo_s == 2){
+
+    # string for total population size:
+    Ns <- rep("", k^2)
+    for(i in 0:(k-1)){
+      for(j in 0:(k-1)){
+        Ns[k*j + i + 1] <- paste0("N", i, j)
+      }
+    }
+    NN <- paste(Ns, collapse = "+")
+    NNb <- paste(Ns[-1], collapse = "+")
+
+    # left sides of equations:
+    leftSides <- paste0("d", Ns)
+
+    # matrix of terms for demography:
+    matD <- matrix(rep(NA, k^2), nrow = k)
+    for(i in 0:(k-1)) {
+      for(j in 0:(k-1)) {
+        matD[i+1, j+1] <- paste0("lambda*N", i, j, "*(1 - (", NN, ")/K) - mu*N", i, j)
+      }
+    }
+    dim(matD) <- NULL
+    # matrix of terms for the  influx force of infection
+
+    # matrix for transmission events:
+
+    matT <- matrix(rep(NA, k^2), nrow = k)
+
+    # total number of species with symbiont A:
+    N_A <- paste(Ns[-(0:(k-1)*k + 1)], collapse = "+")
+    # total number of species with symbiont B:
+    N_B <- paste(Ns[-(1:k)], collapse = "+")
+
+    # calculate pA an pB matrices (inhibition of transmission caused by pre-existing symbionts)
+    matrix0123 <- matrix(rep(0:(k-1), k), nrow = k)
+
+    pA <- matrix(paste0("sigmaA^", matrix0123, "*sigmaBA^", t(matrix0123)), nrow = k)
+    pB <- matrix(paste0("sigmaB^", t(matrix0123), "*sigmaAB^", matrix0123),  nrow = k)
+
+    #matrix of transmission
+    matT <- matrix(rep("", (k)^2), nrow = k, byrow = TRUE)
+    for(i in 0:(k-1)){
+      for(j in 0:(k-1)){
+        if(i>0){ # influx of A symbionts
+          matT[i+1, j+1] <- paste0("+betaA*(", N_A,")*", pA[i, j+1], "*N", i-1, j)
+        }
+        if (j>0){# influx of B symbionts
+          matT[i+1, j+1] <- paste0(matT[i+1, j+1], "+betaB*(", N_B,")*", pB[i+1, j], "*N", i, j-1)
+        }
+        if(i<(k-1)) {#outflux of A endosymbionts
+          matT[i+1, j+1] <- paste0(matT[i+1, j+1], "-betaA*(", N_A,")*", pA[i+1, j+1], "*N", i, j)
+        }
+        if(j<(k-1)){# outflux of B symbionts
+          matT[i+1, j+1] <- paste0(matT[i+1, j+1], "-betaB*(", N_B,")*", pB[i+1, j+1], "*N", i, j)
+        }
+      }
+    }
+    # matrix of terms for outflux due to loss of symbionts:
+    matL <- matrix(rep(NA, k^2), nrow = k)
+    for(i in 0:(k-1)){
+      for(j in 0:(k-1)) {
+        matL[i+1, j+1] <- paste0("(-", i, "*nuA - ", j, "*nuB)*N", i, j)
+      }
+    }
+
+    # matrix of terms for influx due to loss of symbionts:
+    matG <- matrix(rep("", k^2), nrow = k)
+    for(i in 0:(k-1)) {
+      for(j in 0:(k-1)) {
+        if (i<(k-1))
+          matG[i+1, j+1] <- paste0(i+1, "*nuA*N", i+1, j)
+        else
+          matG[i+1, j+1] <- "0"
+        if (j<(k-1))
+          matG[i+1, j+1] <- paste0(matG[i+1, j+1], " + ", j+1, "*nuB*N", i, j+1)
+      }
+    }
+    equations <- paste0("    ", leftSides, " = ", matD, matT, " + ", matL, " + ", matG, "\n")
+    equations <- paste0(equations, collapse = "\n")
+    functionFront <- "ODEsystem <- function(times, states, parameters){\n  with(as.list(c(states, parameters)),{\n"
+    functionEnd <- paste0("\n    return(list(c(", paste0(leftSides, collapse = ", "), ")))\n  })\n}")
+    completeFunction <- paste0(functionFront, equations, functionEnd)
+    cat(completeFunction)
+
+    ODEsystem <- eval(parse(text = completeFunction))
+  }
+
+  #create vector of initial states for the run_model function to use
+    ODE <- list(endo_s = endo_s,
+                endo_no_per_sp = endo_no,
+                equations = ODEsystem,
+                states = Ns)
+    return(ODE)
+}
+
---
+++ R/get_stats.R
@@ -0,0 +1,261 @@
+#' get_stats
+#'
+#' @param results_file : name of file where raw results have been stored. This file must be uploaded to the environment
+#' @param test_parameters : vector of parameters tested.  if NA, all parameters will be tested apart from designated baseline.
+#' additionally, parameters where values are to be matched during analysis can be specified with "etc == etc". Parameters that are able to do defined in this way are: nu, sigma, and beta.
+#' @param set_baseline : vector of parameters to be kept at baseline, if NA, all parameters combinations will be considered.
+#' @param cor_param_method : methods of correlation calculations, options include ld, or
+#' any method from the "cor" function in the stats package.
+#' @param outfile : name of the file results will be saved.
+#'
+#' @return A list containing the following: details of the model including the date of
+#' simulation and parameter combinations, raw results at equilibrium, correlation tests at equilibrium for each test parameter, and if ld is selected linkage disequilibrium at each paramter combination
+#' @export
+#'
+#' @examples
+get_stats <- function(results_file = ODE_results,
+                      test_parameters = c("sigmaBA = sigmaAB"),
+                      set_baseline = c(K = 200, lambda = 1, mu = 0.5),
+                      cor_param_method = c("ld", "pearson"),
+                      outfile = "Stats.rda"){
+
+
+  #split for ease of indexing
+  model_det <- results_file[["simulation_details"]]
+  parameters <- results_file[["param_combos"]]
+  all_results <- results_file[["simulations"]]
+
+
+  #store stats
+
+  #time series variance
+
+
+  # CONSTRUCT TABLES TO STORE DATA
+
+  # stats across time
+  # pastecs stats.desc
+
+  # format dataset to remove all combinations in which specified parameters are not at the baseline
+
+  #returns a subset of the ODE results that are the parameter combinations that are needed.
+
+  #add sigmaA and sigmaB match and sigmaAB and BA match and beta match options
+
+  #subset  the baseline parameters
+  if(length(set_baseline >= 1) && is.na(set_baseline) == FALSE){
+    to_testB <- lapply(all_results, function(x) x[all(set_baseline %in% x$Parameters)])
+    to_testB <- to_testB[lapply(to_testB, length)>0]
+  }else{
+    to_testB<- all_results
+  }
+
+  if(length(to_testB) == 0){
+    warning("Baseline values for parameters you would like to test were not simulated!")
+  }
+
+  #isolating parameter combinations of interest from the dataset
+  if(length(test_parameters >= 1) && is.na(test_parameters) == FALSE){
+    if(model_det$endo_species >= 2){
+      #subset the matched parameters from the test_parameters arguments
+      #create vector to  match
+      test_parameters <- case_when(test_parameters == "sigmaAB = sigmaBA" ~ "sigmaAB",
+                                   test_parameters == "sigmaBA = sigmaAB" ~ "sigmaAB",
+                                   test_parameters == "sigmaA = sigmaB" ~ "sigmaA",
+                                   test_parameters == "sigmaB = sigmaA" ~ "sigmaA",
+                                   test_parameters == "nuA = nuB" ~ "nuA",
+                                   test_parameters == "nuB = nuA" ~ "nuA")
+
+      to_test <- lapply(to_testB, function(x) x[unique(x$Parameters[,test_parameters]) %in% x$Parameters])
+      #to_test <- lapply(to_testB, function(x) x[test_parameters %in% unique(x$Parameters[ test_parameters])])
+      to_test <- to_test[lapply(to_test, length)>0]
+    }else{
+      to_test <- to_testB
+    }
+  }
+  if(length(to_test) == 0){
+    warning("Parameter combinations you would like to test were not simulated!")
+  }
+
+  # stats at equilibrium #
+
+  # raw results for each parameter at equilibrium
+  colength <- rep(NA, ncol(to_test[[1]][["Results"]])+ncol(parameters))
+  eq_raw <- as.data.frame(matrix(NA,nrow = length(to_test),
+                                 ncol= length(colength)))
+  colnames(eq_raw) <- c(colnames(parameters),
+                        colnames(to_test[[1]]$Results))
+
+  #fill in parameters and and  infection status
+  if(model_det$endo_species >= 2){
+    eq_infect <- c("N00","A", "B", "A_plus", "B_plus", "coinf")
+  }else{
+    eq_infect <- c("N00","A", "A_plus")
+  }
+
+  # fill in the tables
+
+  # time till equilibrium
+
+  # checks variance across time point, coefficient of variance.
+
+  # coefficient of variance
+  # coef <- function(X){
+  #   (sd(X)/mean(X))*1000
+  # }
+
+
+
+
+ # finding the time system reaches stable equilibrium  and fill in the raw results
+
+  print(paste(" Finding timepoint where stable equilibrium is reached"))
+  for(i in 1:length(to_test)){
+    s <- to_test[[i]][["Results"]]
+    p <- to_test[[i]][["Parameters"]]
+    for(k in 1:(nrow(s)-1)){
+      eq <- as.matrix(s[k:(k+1), ])
+      VarC <- apply(eq[,-1], 2, FUN = var)
+      if(all(VarC <= 0.1, na.rm = T)){
+        eq_raw[i,] <- c(p, s[eq[1,1],])
+        break
+      }
+    }
+    if(all(is.na(eq_raw))){
+      warning("system did not reach a stable equilbrium at maximum timestep
+          (", model_det$max_timesteps, ")! \n statistics at equilibrium will not be calculated")
+    }
+  }
+
+
+  # function to calculate the proportion at equilibrium
+  prop <- function(X){
+    X/(p$K*p$mu)
+  }
+
+
+  #proportion co-infected
+  if(model_det$endo_species >= 2){
+    coinf <- c(rowSums(eq_raw[, grep("^[^0slnbtKm]*$", colnames(eq_raw))]))
+    #create vector that will be used later on for the pivot longer
+    #prop single B
+    B <- prop(eq_raw$N01)
+    # double B
+    #string pattern needed to recognise columns for species B co-infection
+    patB <- rep(NA, model_det$endo_no_per_sp)
+    for(i in 2:model_det$endo_no_per_sp){
+      patB[i] <- c(paste0("N0", i))
+    }
+    #eq_2B <-eq_raw[eq_raw %in% patB]
+    patB <- na.omit(patB)
+    doubleB_prop <- c(prop(rowSums(eq_raw[eq_raw %in% patB])))
+  }
+  #prop single A
+  A <- prop(eq_raw$N10)
+
+
+  # single species co-infections
+
+  #string pattern needed to recognise columns for species A co-infections
+  patA <- rep(NA, model_det$endo_no_per_sp)
+  for(i in 2:model_det$endo_no_per_sp){
+    patA[i] <- c(paste0("N", i, "0"))
+  }
+  patA <- na.omit(patA)
+  doubleA_prop <- c(prop(rowSums(eq_raw[eq_raw %in% patA])))
+
+  #prop_uninfected
+  N00 <- prop(eq_raw[, "N00"])
+  if(model_det$endo_species >=2){
+    eq_dat <- cbind(eq_raw, data.frame("N00" = N00,
+                                       "A" = A,
+                                       "B" = B,
+                                       "A_plus" = doubleA_prop,
+                                       "B_plus" = doubleB_prop,
+                                       "coinf" = coinf))
+  }else{
+    eq_dat <- cbind(eq_raw, data.frame("N00" = N00, "A" = A,
+                                       "A_plus" = doubleA_prop))
+  }
+
+
+
+  # equilibrium proportions
+  if(is.na(test_parameters) == TRUE){
+    par <- colnames(parameters)
+    eq_met <- pivot_longer(eq_dat, all_of(par), names_to = "parameter")
+  } else{
+    eq_met <- pivot_longer(eq_dat, all_of(test_parameters), names_to = "parameter")
+  }
+
+  eq_met <- pivot_longer(eq_met,eq_infect,
+                         names_to = "infection_status", values_to = "proportion")
+  eq_av_prop <- eq_met[, c("parameter", "value", "infection_status", "proportion")]
+  eq_av_prop <- eq_av_prop[order(eq_av_prop$parameter, decreasing = FALSE), ]
+
+  #calculating correlation coefficients
+
+
+  # linkage disequilibrium
+
+  ld <- function(coinf, N0, A, B, A_plus = 0, B_plus = 0){
+    if(model_det$endo_no_per_sp >= 2){
+      coinf*N0 - sum(A, A_plus)*sum(B, B_plus)
+    } else{
+      coinf*N0 - A*B
+    }
+  }
+  #add levels to dataset
+
+  #group_by?
+
+  eq_av_prop$parameter <- factor(eq_av_prop$parameter)
+  eq_av_prop$infection_status <- factor(eq_av_prop$infection_status)
+
+
+print(paste("Calculating correlations at equilibrium"))
+
+  #calculate correlation coefficients.
+  if(length(cor_param_method) >= 1){
+    if(any(cor_param_method == "ld")){
+      corT <- cor_param_method[grep("^[^ld]", cor_param_method)]
+      eq_param_cor <- eq_av_prop %>% group_by(parameter, infection_status) %>% summarise(cor = cor(value, proportion, method = corT))
+      eq_ld <- data.frame("ld" = NA)
+      cols <- c(colnames(to_test[[1]]$Parameters))
+      eq_p <- data.frame(matrix(ncol = length(to_test[[1]]$Parameters)))
+      colnames(eq_p) <- cols
+      for(i in 1:length(to_test)){
+        eq_p[i,] <- to_test[[i]][["Parameters"]]
+        eq_ld[i,] <-ld(coinf = eq_dat$coinf[i],
+                       N0 = eq_dat$N00[i],
+                       A = eq_dat$A[i],
+                       B = eq_dat$B[i],
+                       A_plus = eq_dat$A_plus[i],
+                       B_plus = eq_dat$B_plus[i])
+
+      }
+      eq_ld <- cbind(eq_p, eq_ld)
+    }else{
+      eq_param_cor <- eq_av_prop %>% group_by(parameter, infection_status) %>% summarise(cor = cor(value, proportion, method = cor_param_method))
+    }
+    #prep file for saving
+    if(any(cor_param_method == "ld")){
+      Stats <- list(model_det = model_det,
+                    "equilibrum_stats" = list("Results" = eq_raw,
+                                              "Proportion" = eq_av_prop ,
+                                              "correlations" = eq_param_cor,
+                                              "ld" = eq_ld))
+    }else{
+      Stats <- list("model_det" = model_det,
+                    "equilibrum_stats" = list("Results" = eq_raw,
+                                              "Proportion" = eq_av_prop ,
+                                              "correlation" = eq_param_cor))
+    }
+
+    # save file
+    save(Stats, file = outfile)
+
+    load(outfile, .GlobalEnv)
+  }
+}
+
