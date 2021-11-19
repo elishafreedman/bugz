b7e937adf13b15a2e7fbdc0f1b938a06cbadae57
Elisha Freedman <freedmanelisha@gmail.com>
2021-11-19 12:39:11 +1000

    Update get_equil_points.R

--- R/get_equil_points.R
+++ R/get_equil_points.R
@@ -23,20 +23,21 @@ get_equi_points<- function(results_file = ODE_results, eq_threshold = 0.1){
   # #percent of variance
   #
   # perVar <- function(X){
-  #  abs((X[2] - X[1])/X[1])
+  #  abs('(X[2] - X[1])/X[1])
   # }
 
-  isEqui <- function(x, eq_threshold){
+  isEqui <- function(x, eq_thresh = eq_threshold){
     x1 <- round(x[1], 2)
     x2 <- round(x[2], 2)
-    max(abs(x1-x2)/x1, na.rm = TRUE)<eq_threshold
+    max(abs(x1-x2)/x1, na.rm = TRUE)<= eq_thresh
   }
   # finding the time system reaches stable equilibrium  and fill in the raw results
 
 
 
 
-  eq_raw <- data.frame()
+  eq_raw <- data.frame(matrix(nrow = nrow(parameters), ncol = nrow(parameters)+nrow(all_results[[1]][["Results"]])))
+   colnames(eq_raw) <- c(colnames(parameters), colnames(all_results[[1]][["Results"]]))
 
   print(paste("Finding timepoint where stable equilibrium is reached"))
   for(i in 1:length(all_results)){
@@ -45,12 +46,14 @@ get_equi_points<- function(results_file = ODE_results, eq_threshold = 0.1){
     for (k in 1:(nrow(s) - 1)){
       eq <- as.matrix(s[k:(k + 1),])
       VarC <- apply(eq[, -1], 2, FUN = isEqui)
-      if (all(VarC <= eq_threshold, na.rm = T)){
+      #print(VarC)
+      if (all(VarC) == TRUE){
         eq_raw[i, ] <- c(p, s[k + 1, ])
         break
       }
     }
     print(paste("search for equilbrium at combination", i, "done"))
+  }
     if (all(is.na(eq_raw))){
       warning(
         "system did not reach a stable equilbrium at maximum timestep
@@ -59,7 +62,7 @@ get_equi_points<- function(results_file = ODE_results, eq_threshold = 0.1){
         ")! \n statistics at equilibrium will not be calculated"
       )
     }
-  }
+
   #prep file for saving
     return(list(
       model_det = model_det,
